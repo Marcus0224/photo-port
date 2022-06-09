b0edc394f87f9bd83fd238f675fbb09319f104af
Marcus0224 <marcstlouis04@gmail.com>
2022-06-08 20:38:35 -0400

    modal closed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import coverImage from "../../assets/cover/cover-image.jpg";
-
+import React from 'react';
+import coverImage from '../../assets/cover/cover-image.jpg';
 function About() {
-
   return (
-    <section className="my-5" >
+    <section className="my-5">
       <h1 id="about">Who am I?</h1>
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       <div className="my-2">
         <p>
-          Love is love .
-      </p>
+          Love is love 
+        </p>
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
